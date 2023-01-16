6019423e67a500983bdd88b46c98c0439a9decf8
banzzagi <junsang819@hanyang.ac.kr>
2023-01-16 13:41:11 +0900

    .

--- 재귀/11729번 하노이 탑 이동 순서.py
+++ 재귀/11729번 하노이 탑 이동 순서.py
@@ -1,42 +1,10 @@
-def hanoi1(n):
+n = int(input())
+def hanoi(n, a, b, c):
     if n == 1:
-        return 1  
+        print(a,c)
     else:
-        return hanoi1(n-1)*2 + 1
-def hanoi2(n):
-    if n == 1:
-        A = [[1,3]]
-        return [A[0][0], A[0][1]]
-    else:
-        A = hanoi2(n-1)
-        B = A
-        C = A
-        D = []
-        E = [0]*len(A)
-        F = [0]*len(A)
-        for i in B:
-            for index, value in enumerate(B):
-                if value == 3:
-                    B[index] = 2
-                    E[index] = 1
-                if value == 2 and E[index] != 1:
-                    B[index] = 3
-        for i in C:
-            for index, value in enumerate(C):
-                if value == 1:
-                    C[index] = 2
-                    F[index] = 1
-                if value == 2 and F[index] != 1:
-                    C[index] = 1
-        for i in B:
-            D.append(i)
-        D.append([1,3])
-        for i in C:
-            D.append(i)    
-
-        return D
-
-K = int(input())
-print(hanoi1(K))
-print(hanoi2(K))
-
+        hanoi(n-1,a,c,b)
+        print(a,c)
+        hanoi(n-1,b,a,c)
+print(2**n-1)
+hanoi(n,1,2,3)
